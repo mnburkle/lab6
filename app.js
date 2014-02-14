322dece95140c3e9c5f7eb3663a27ebf99513abd
mnburkle <mnburkle@stanford.edu>
2014-02-14 11:50:04 -0800

    fixed app.js

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
